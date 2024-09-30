764eb8d73f8f3a45c8ea6bee675b05515df07dd2
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2024-09-30 19:33:10 -0300

    Numeros e variaveis

---
+++ tipos_variaveis/numeros.py
@@ -0,0 +1,42 @@
+#Inteiro
+numero_inteiro = 3
+print(f'Inteiro = {numero_inteiro}')
+
+# Real com ponto flutuante
+numero_real = 3.5
+print(f'Real com ponto flutuante = {numero_real}')
+
+#Type()
+print(f"Tipo da variável inteiro = {type(numero_inteiro)}")
+print(f"Tipo da variável real = {type(numero_real)}")
+
+#Soma +
+soma = 1 + 1
+print(f"1 + 1 = {soma}")
+
+#Subtração -
+subtracao = 1 - 1
+print(f"1 - 1 = {subtracao}")
+
+#Multiplicação *
+multiplicacao = 2 * 2
+print(f"2 * 2 = {multiplicacao}")
+
+#Divisão /
+divisao = 5 / 2
+print(f"5 / 2 = {divisao}")
+print(f"Tipo da variável da divisão {type(divisao)}")
+
+# int()
+print(f"O valor por inteiro = {int(divisao)}")
+
+# float()
+print(f"Valor em float = {float(divisao)}")
+
+# Modulo %
+modulo = 5 % 2
+print(f"5 % 2 = {modulo}")
+
+#Divisão Inteiro //
+divisao = 5 // 2
+print(f"5 // 2 = {divisao}")
