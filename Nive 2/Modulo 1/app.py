b72f6dc3d4b082f877e05e5f8b1281449d75df7f
Rodrigo Takara <rodrigo.takara1505@gmail.com>
2024-10-18 19:03:59 -0300

    🏁 CRUD com Flask e Postman

--- Nive 2/Modulo 1/app.py
+++ Nive 2/Modulo 1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from Models.task import Task
 
 
@@ -9,13 +9,52 @@ app = Flask(__name__)
 
 tasks = []
 
+task_id_count = 1
+
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_count
     data = request.get_json()
-    print(data)
-    return "test"
+    new_task = Task(id = task_id_count, title = data.get('title'), description= data.get('description', ''))
+    tasks.append(new_task)
+    task_id_count += 1
+    print(tasks)
+    return jsonify({
+        "Message":"Nova tarefa criada com sucesso"
+        })
+
+@app.route('/tasks', methods = ['GET'])
+def get_tasks():
+    list_task = [task.to_dict() for task in tasks]
+
+    output = {
+        "tasks" : list_task,
+        "total_tasks" : len(list_task)
+    }
+    
+    return output
+
+@app.route('/tasks/<int:id>', methods = ["GET"])
+def get_task_by_id(id):
+    for task in tasks:
+        if task.id == id:
+            return jsonify(task.to_dict())
+    return jsonify({"Message":"Arquivo não encontrado"}), 404
 
 
+@app.route('/tasks/<int:id>', methods = ["PUT"])
+def update_task(id):
+    for task in tasks:
+        if task.id == id:
+            data = request.get_json()
+
+            task.title = data.get('title')
+            task.description = data.get('description')
+            task.completed = data.get('completed')
+
+            return jsonify({"Message": "Tarefa atualizada com sucesso!"})
+
+    return jsonify({"Message": "Arquivo não encontrado"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
