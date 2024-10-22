3b1bbd3bea1804d9e206797d1bc703251d6fd1d0
Rodrigo Takara <rodrigo.takara1505@gmail.com>
2024-10-22 19:47:29 -0300

    🟢 app.py - Login e Logout

--- 2 - Nivel 2/Projeto/app.py
+++ 2 - Nivel 2/Projeto/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, current_user, login_user
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+import os
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance/database.db')
 
 db.init_app(app)
 login_manager = LoginManager()
@@ -16,36 +17,38 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-  return User.query.get(user_id)
-
+    return User.query.get(user_id)
 
 @app.route('/login', methods=['POST'])
 def login():
-
     data = request.json
-
     username = data.get('username')
     password = data.get('password')
-
-    if username and password:       
-
+    
+    if username and password:
         user = User.query.filter_by(username=username).first()
-
         if user and user.password == password:
             login_user(user)
-            
-            print(current_user.username.is_authenticated)
+            print(current_user.is_authenticated)
             return jsonify({'message':'Usuário credenciado'})
-       
+
+    return jsonify({'message':'Usuário não encontrado'}), 400
 
 
-    return jsonify({'message':'Usuario não encontrado'}), 400
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Usuário deslogado com sucesso'})
 
-
-   
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
     return "Hello world"
+
+
+# Cria as tabelas ao iniciar a aplicação
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- 2 - Nivel 2/Projeto/models/user.py
+++ 2 - Nivel 2/Projeto/models/user.py
@@ -2,6 +2,8 @@ from database import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(80), nullable=False, unique = True)
-    password = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)  # Aumentado o tamanho para suportar senhas hash
+
+    # Método para serializar o objeto User em um dicionário
