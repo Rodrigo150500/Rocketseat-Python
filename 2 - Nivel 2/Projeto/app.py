59236e7e6b70b8b07ab45f4d8797c0c9394a5e33
Rodrigo Takara <rodrigo.takara1505@gmail.com>
2024-10-22 17:30:39 -0300

    🟡 app.py

--- 2 - Nivel 2/Projeto/app.py
+++ 2 - Nivel 2/Projeto/app.py
@@ -1,14 +1,48 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager, current_user, login_user
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db.init_app(app)
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
 
 
+@app.route('/login', methods=['POST'])
+def login():
+
+    data = request.json
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:       
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+
+          
+            return jsonify({'message':'Usuário credenciado'})
+       
+
+
+    return jsonify({'message':'Usuario não encontrado'}), 400
+
+
+   
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
     return "Hello world"
