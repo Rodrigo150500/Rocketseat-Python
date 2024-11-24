09aba6d24b9edcea1f89bd0c75f3e137830f52b4
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2024-11-24 18:31:14 -0300

    🗑 Deleting files

--- 1 - Nível 1/Modulo 1/modulo_terceiro.py
+++
@@ -1,6 +0,0 @@
-print("\nImportação e uso de um módulo de terceiro")
-import requests
-
-url = "http://www.example.com"
-response = requests.get(url)
-print(f"Solicitação HTTP para {url} retornou o status {response.status_code}")
