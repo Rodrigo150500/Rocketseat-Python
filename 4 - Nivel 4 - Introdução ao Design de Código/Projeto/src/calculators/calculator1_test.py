7192a61d70dd8f80613080ead62c64d9c7fbdf9a
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2024-12-11 18:19:41 -0300

    Add failed test

--- 4 - Nivel 4 - Introdução ao Design de Código/Projeto/src/calculators/calculator1_test.py
+++ 4 - Nivel 4 - Introdução ao Design de Código/Projeto/src/calculators/calculator1_test.py
@@ -1,11 +1,12 @@
 from calculators.calculator1 import Calculator1
 from typing import Dict
+from pytest import raises
 
 class MockRequest:
     def __init__(self, body: Dict):
         self.json = body
 
-def test_fomart():
+def test_calc():
     request = MockRequest(body={"number": 5})
 
     calc = Calculator1()
@@ -19,3 +20,15 @@ def test_fomart():
     #Testando resultado
     assert response['data']['Calculator'] == 1
     assert response['data']['Result'] == 17.39
+
+def test_calculate_with_bodu_error():
+
+    request = MockRequest(body= {"something": 5})
+
+    calc = Calculator1()
+
+    with raises(Exception) as exception:
+        calc.calculate(request)
+    
+    assert str(exception.value) == "body mal formatado!"
+    
