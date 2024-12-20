c938de20a65c26977295f083d851608ed351ea8b
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2024-12-20 20:08:41 -0300

    Create concept file of O from S.O.L.I.D

---
+++ 4 - Nivel 4 - Introdução ao Design de Código/SOLID/2_O_OCP/ocp_example.py
@@ -0,0 +1,31 @@
+#Usando deste método não segue o princípio do aberto/fechado
+class Company:
+    def do_word(self, work: int):
+        if(work == 1):
+            print("Sellers are raising money")
+        elif(work == 2):
+            print("HR are hiding new devs")
+        elif(work == 3):
+            print("Devs are coding")
+        else:
+            print("No work")
+
+#Seguindo o princípio do aberto/fechado podemos ver que muda a forma que entramos o dado, porém mantemos sua estrutura.
+class Programmer:
+    def make(self):
+        print("Programmers are coding")
+
+class RH:
+    def make(self):
+        print("HR are hiding")
+
+class SCompany:
+    def do_work(self, work: any):
+        work.make()
+
+programmer = Programmer()
+rh = RH()
+company = SCompany()
+
+company.do_work(programmer)
+company.do_work(rh)
