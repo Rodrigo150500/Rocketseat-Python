de3d16f30ce7133437456ca57d210fe8345a890e
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2024-12-24 23:25:13 -0300

    Create Pylint Example

---
+++ 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/ex_pylint.py
@@ -0,0 +1,12 @@
+'''Alguma coisa sobre o documento'''
+
+print("Ola Mundo")
+
+def minha_funcao():
+    '''Alguma coisa sobre o documento'''
+
+    print("Minha funcao")
+
+#Snake_case: Variaveis e Funções minha_funcao
+#camera_case: Não adota minha_funcao
+#Pascade_case: Classses MinhaFuncao
