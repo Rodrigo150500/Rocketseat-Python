0ace744086d13614ebc783b6500aa90bff954ac0
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2024-12-25 20:47:20 -0300

    commit example

--- 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/ex_pylint.py
+++ 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/ex_pylint.py
@@ -1,12 +1,12 @@
+"""Alguma coisa do codigo"""
 
-
-print("Ola Mundo")
 
 def minha_funcao():
-    '''Alguma coisa sobre o documento'''
+    """Alguma coisa sobre o documento"""
 
     print("Minha funcao")
 
-#Snake_case: Variaveis e Funções minha_funcao
-#camera_case: Não adota minha_funcao
-#Pascade_case: Classses MinhaFuncao
+
+# Snake_case: Variaveis e Funções minha_funcao
+# camera_case: Não adota minha_funcao
+# Pascade_case: Classses MinhaFuncao
