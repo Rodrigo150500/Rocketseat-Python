82387e6d239b0356401e4aa88245681959dcb2af
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2024-12-25 23:12:57 -0300

    Teste

--- 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/ex_pylint.py
+++ 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/ex_pylint.py
@@ -3,6 +3,7 @@
 def minha_funcao():
    
     print("Minha funcao")
+    print("Ola mundo")
 # Snake_case: Variaveis e Funções minha_funcao
 # camera_case: Não adota minha_funcao
 # Pascade_case: Classses MinhaFuncao
