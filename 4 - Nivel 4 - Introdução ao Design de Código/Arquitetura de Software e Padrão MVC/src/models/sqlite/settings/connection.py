8437aef3580c909d837e70a5d3d9e56a3e240e3a
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-01-01 15:49:38 -0300

    Conect to database

--- 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/models/sqlite/settings/connection.py
+++ 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/models/sqlite/settings/connection.py
@@ -1,16 +1,28 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-class DBConnectionHandle:
+class DBConnectionHanlder:
     def __init__(self) -> None:
-        self.__connction_string = "sqlite:///storage.db"
+        self.__conection_string = "sqlite:///C:/Users/Rodrigo/Desktop/Rocketseat-Python/4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/storage.db"
+
         self.__engine = None
-    
+        self.session = None
+
     def conect_to_db(self):
-        self.__engine = create_engine(self.__connction_string)
+        self.__engine = create_engine(self.__conection_string)
 
     def get_engine(self):
         return self.__engine
 
+    def __enter__(self):
+        session_maker = sessionmaker()
 
-db_connect_handler = DBConnectionHandle()
+        self.session = session_maker(bind=self.__engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+
+
+db_connect_handler = DBConnectionHanlder()
