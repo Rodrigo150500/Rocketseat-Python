5eb21de935510e3d94be1d9ea5c87e922f758123
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-01-01 22:16:11 -0300

    Add delete method for pet table

--- 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/models/sqlite/repository/repository_test.py
+++ 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/models/sqlite/repository/repository_test.py
@@ -1,12 +1,22 @@
+import pytest
 from ..settings.connection import db_connect_handler
 from .pets_repository import PetsRepository
 
 db_connect_handler.conect_to_db()
 
+@pytest.mark.skip(reason="Interação com Banco de Dados")
 def test_list_pets():
     repository = PetsRepository(db_connect_handler)
 
     response = repository.list_pets()
 
     print()
-    print(response)
+    print(response) 
+
+def test_delete_pet():
+
+    name = "belinha"
+    
+    repository = PetsRepository(db_connect_handler)
+
+    repository.delete_pet(name)
