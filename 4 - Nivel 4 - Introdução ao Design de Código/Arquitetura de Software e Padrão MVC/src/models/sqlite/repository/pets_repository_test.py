956043d33556b999c38a100c2a78f08880e3d1b4
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-01-11 07:25:26 -0300

    Test: Finishing tests repositories

--- 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/models/sqlite/repository/pets_repository_test.py
+++ 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/models/sqlite/repository/pets_repository_test.py
@@ -1,5 +1,7 @@
 from unittest import mock
+import pytest
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+from sqlalchemy.orm.exc import NoResultFound
 from ..entities.pets import PetsTable
 from .pets_repository import PetsRepository
 
@@ -18,12 +20,27 @@ class MockConnection:
             ]
         )
     
+    
     def __enter__(self): 
         return self
     
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
 
+class MockConnectionNoResult:
+
+    def __init__(self) -> None:
+        self.session = UnifiedAlchemyMagicMock()
+        self.session.query.side_effect = self.__raise_no_result_found
+    
+    def __raise_no_result_found(self, *args, **kwargs):
+        raise NoResultFound("No result found")
+    
+    def __enter__(self): 
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 def test_list_pets():
     mock_connection = MockConnection()
@@ -38,6 +55,19 @@ def test_list_pets():
 
     assert response[0].name == "dog"
 
+def test_list_pets_no_result():
+    mock_connection = MockConnectionNoResult()
+
+    connection = PetsRepository(mock_connection)
+
+    response = connection.list_pets()
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.all.assert_not_called()
+    mock_connection.session.filter.assert_not_called()
+
+    assert response == []
+
 def test_delete_pet():
 
     mock_connection = MockConnection()
@@ -49,3 +79,14 @@ def test_delete_pet():
     mock_connection.session.query.assert_called_once_with(PetsTable)
     mock_connection.session.filter.assert_called_once_with(PetsTable.name == "nome")
     mock_connection.session.delete.assert_called_once()
+
+def test_delete_pet_no_result():
+
+    mock_connection = MockConnectionNoResult()
+
+    repo = PetsRepository(mock_connection)
+
+    with pytest.raises(Exception):
+        repo.delete_pet("nome")
+        
+    mock_connection.session.rollback.assert_called_once()
