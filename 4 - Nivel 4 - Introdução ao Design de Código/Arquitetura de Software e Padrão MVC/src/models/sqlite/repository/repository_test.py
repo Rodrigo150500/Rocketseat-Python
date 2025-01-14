0b86d1d32811a2329051b62fe75c58db4f0b5d98
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-01-14 06:45:16 -0300

    Implementing in peopleRepository

--- 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/models/sqlite/repository/repository_test.py
+++ 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/models/sqlite/repository/repository_test.py
@@ -1,6 +1,7 @@
 import pytest
 from ..settings.connection import db_connect_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 db_connect_handler.conect_to_db()
 
@@ -13,6 +14,7 @@ def test_list_pets():
     print()
     print(response) 
 
+@pytest.mark.skip(reason="Interação com Banco de Dados")
 def test_delete_pet():
 
     name = "belinha"
@@ -20,3 +22,28 @@ def test_delete_pet():
     repository = PetsRepository(db_connect_handler)
 
     repository.delete_pet(name)
+
+@pytest.mark.skip(reason="Interação com Banco de Dados")
+def test_insert_person():
+    first_name = "First Name"
+    last_name = "Last Name"
+    age = 18
+    pet_id = 2
+
+    repo = PeopleRepository(db_connect_handler)
+
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+def test_get_person():
+
+    person_id = 1
+
+    repo = PeopleRepository(db_connect_handler)
+
+    response = repo.get_person(person_id)
+
+    print()
+
+    print(response)
+
+    print(response.pet_name)
