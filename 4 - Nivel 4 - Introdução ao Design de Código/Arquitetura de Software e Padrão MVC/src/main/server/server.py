28007d4e56d70f78b11063ab1d321c0af5978c68
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-01-22 06:42:38 -0300

    Feat: Implementing project routes for pet and people

--- 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/main/server/server.py
+++ 4 - Nivel 4 - Introdução ao Design de Código/Arquitetura de Software e Padrão MVC/src/main/server/server.py
@@ -4,6 +4,8 @@ from src.models.sqlite.settings.connection import db_connect_handler
 
 #Importar blueprints
 from src.main.routes.pets_routes import pet_route_bp
+from src.main.routes.person_routes import person_route_bp
+
 
 db_connect_handler.conect_to_db()
 
@@ -11,5 +13,6 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(pet_route_bp)
+app.register_blueprint(person_route_bp)
 
 
