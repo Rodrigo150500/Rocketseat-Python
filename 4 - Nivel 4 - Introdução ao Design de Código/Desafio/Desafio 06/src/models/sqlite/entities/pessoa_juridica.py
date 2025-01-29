ff7a17ef394a4d04ae5e9282468b5f5a2f2490d9
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-01-29 06:48:28 -0300

    config: Implementing entities in models for sqlite

---
+++ 4 - Nivel 4 - Introdução ao Design de Código/Desafio/Desafio 06/src/models/sqlite/entities/pessoa_juridica.py
@@ -0,0 +1,19 @@
+from src.models.sqlite.settings.base import Base
+from sqlalchemy import Column, BIGINT, REAL, String
+
+class PessoaJuridica(Base):
+
+    __tablename__ = "pessoa_juridica"
+
+    id = Column(BIGINT, primary_key=True)
+    faturamento = Column(REAL, nullable=False)
+    idade = Column(BIGINT, nullable=False)
+    nome_fantasia = Column(String, nullable=False)
+    celular = Column(String, nullable=False)
+    email_corporativo = Column(String, nullable=False)
+    categoria = Column(String, nullable= False)
+    saldo = Column(REAL, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Pessoa Juridica [nome_fantasia = {self.nome_fantasia}, idade = {self.idade}]"
+    
