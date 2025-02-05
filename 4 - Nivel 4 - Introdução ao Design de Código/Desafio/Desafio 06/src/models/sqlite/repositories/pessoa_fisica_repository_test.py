b6cda52026feca68bfb44f515b2ba41d30bfa205
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-02-05 06:16:55 -0300

    Feat: Implementing unit test for pessoa fisica, add test_listar_usuarios function

--- 4 - Nivel 4 - Introdução ao Design de Código/Desafio/Desafio 06/src/models/sqlite/repositories/pessoa_fisica_repository_test.py
+++ 4 - Nivel 4 - Introdução ao Design de Código/Desafio/Desafio 06/src/models/sqlite/repositories/pessoa_fisica_repository_test.py
@@ -1,4 +1,50 @@
 from src.models.sqlite.repositories.pessoa_fisica_repository import PessoaFisicaRepository
+from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+from unittest import mock
+from src.models.sqlite.entities.pessoa_fisica import PessoaFisicaTable
 
-def test_pessoa_fisica_repository():
-    pass
+class MockConnection:
+
+    def __init__(self):
+        self.session = UnifiedAlchemyMagicMock(
+            data = [
+
+                (           
+                    [mock.call.query(PessoaFisicaTable)],
+                    [PessoaFisicaTable(renda_mensal = 15000, 
+                                        idade= 35,
+                                        nome_completo = "Diguinho",
+                                        celular = "(59) 91657-4878",
+                                        email = "Diguinho@gmail.com",
+                                        categoria = "Categoria B",
+                                        saldo = 15.90),
+                    PessoaFisicaTable(renda_mensal = 1674, 
+                                        idade= 23,
+                                        nome_completo = "Murilo",
+                                        celular = "(45) 97845-4598",
+                                        email = "Murilo@gmail.com",
+                                        categoria = "Categoria A",
+                                        saldo = 65.99)]
+                )
+            ]
+        )
+    
+    def __enter__(self): return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb): pass
+
+
+
+
+def test_listar_usuarios():
+    mock_connection = MockConnection()
+
+    repo = PessoaFisicaRepository(mock_connection)
+
+    response = repo.listar_usuarios()
+
+    mock_connection.session.query.assert_called_once_with(PessoaFisicaTable)
+    mock_connection.session.all.assert_called_once()
+    mock_connection.session.filter.assert_not_called()
+
+    assert response[0].nome_completo == "Diguinho"
