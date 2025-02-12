df1a5ebf122c4efc1768db5fb4351b940b8d33e9
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-02-12 10:53:10 -0300

    Feat: Implementing user repository with connection (need tests)

---
+++ 5 - Nivel 5 - Autenticação JWT e Segurança/src/models/repositories/user_repository.py
@@ -0,0 +1,48 @@
+from sqlite3 import Connection
+
+class UserRepository:
+
+    def __init__(self, conn: Connection) -> None:
+        self.__conn = conn
+
+    def registry_user(self, username: str, password: str) -> None:
+
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            """
+                INSERT INTO users 
+                    (username, password, balance)
+                VALUES
+                    (?, ?, ?);
+            """, (username, password, 0)
+        )
+
+        self.__conn.commit()
+
+
+    def edit_balance(self, userID: int, balance: float) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            """
+                UPDATE users
+                SET balance = ?
+                WHERE id = ?
+            """, (balance, userID)
+        ) 
+
+        self.__conn.commit()
+    
+    def get_user_by_username(self, username: str) -> tuple:
+
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            """
+                SELECT id, username, password, balance
+                FROM users
+                WHERE username = ?
+            """, (username,)
+        )
+
+        user = cursor.fetchone()
+
+        return user
