2834875943ba763bfd5247c726b9dcd640ea7ed0
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-03-15 05:53:59 -0300

    Feat: Implementing error test in login controller

--- 5 - Nivel 5 - Autenticação JWT e Segurança/Desafio/src/controllers/login_controller_test.py
+++ 5 - Nivel 5 - Autenticação JWT e Segurança/Desafio/src/controllers/login_controller_test.py
@@ -1,7 +1,38 @@
+import pytest
 from .login_controller import LoginController
 from src.drivers.password_handler import PasswordHandler
 
 class MockRepository:
+
+    def encrypt_password(self):
+        
+        password = "ABC123"
+        
+        hashed_password = PasswordHandler().encrypt_password(password)
+       
+        return hashed_password
+
+    def get_user_by_username(self, username):
+
+        hashed_password = self.encrypt_password()
+        
+        return (1, 'Rodrigo', hashed_password)
+
+class MockRepositoryUserNotFound:
+
+    def encrypt_password(self):
+        
+        password = "ABC123"
+        
+        hashed_password = PasswordHandler().encrypt_password(password)
+        
+        return hashed_password
+
+    def get_user_by_username(self, username):
+
+        return 
+
+class MockRepositoryErrorPassword:
 
     def encrypt_password(self):
         
@@ -29,4 +60,22 @@ def test_login():
     assert response["data"]["username"] == "Rodrigo"
     assert response["data"]["token"] is not None
 
-    
+def test_login_user_not_found():
+
+    repo = MockRepositoryUserNotFound()
+
+    controller = LoginController(repo)
+
+    with pytest.raises(Exception):
+        controller.login("Rodrigo", "ABC123")
+
+def test_login_user_error_password():
+
+    repo = MockRepositoryErrorPassword()
+
+    controller = LoginController(repo)
+
+    with pytest.raises(Exception):
+        controller.login("Rodrigo", "ABC1234")
+
+
