bf99ff138095c0af9d0b0b79f43d9bec4ac090c8
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-04-12 05:52:41 -0300

    Feat: Implementing http types

--- 7 - Nivel 7 - NoSQL com MongoDB/src/main/routes/delivery_routes.py
+++ 7 - Nivel 7 - NoSQL com MongoDB/src/main/routes/delivery_routes.py
@@ -1,8 +1,10 @@
 from flask import request, jsonify, Blueprint
-
+from src.main.http_types.http_request import HttpRequest
+from src.main.http_types.http_response import HttpResponse
 delivery_routes_bp = Blueprint("delivery_routes", __name__)
 
 @delivery_routes_bp.route("/delivery/order", methods=["POST"])
 def registry_order():
-    print(request.json)
+    http_request = HttpRequest(body=request.json)
+    print(http_request)
     return jsonify({"ola":"mundo"}),200
