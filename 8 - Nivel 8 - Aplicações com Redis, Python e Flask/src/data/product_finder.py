ee22e2f83490a2d1f634553373939ac8e3541ece
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-05-17 05:49:32 -0300

    Feat: Implementing http types

--- 8 - Nivel 8 - Aplicações com Redis, Python e Flask/src/data/product_finder.py
+++ 8 - Nivel 8 - Aplicações com Redis, Python e Flask/src/data/product_finder.py
@@ -1,8 +1,12 @@
 from src.models.redis.interface.redis_repository import RedisRepositoryInterface
 from src.models.sqlite.interface.products_repository import ProductsRepositoryInterface
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
 
 class ProductFinder:
     def __init__(self, redis_repo: RedisRepositoryInterface, products_repo: ProductsRepositoryInterface):
         self.__repo_redis = redis_repo
         self.__products_repo = products_repo
-        
+    
+    def find_by_name(self, http_request: HttpRequest) -> HttpResponse:
+        pass
